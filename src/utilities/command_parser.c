a200b6764148e14b3e8fd2c51ec0f471e473ce87
mohamed shaapan <mohamed.shaapan.1@gmail.com>
2017-10-08 20:39:08 +0000

    substituting vars in arg list with actual values added

--- src/utilities/command_parser.c
+++ src/utilities/command_parser.c
@@ -1,6 +1,7 @@
 // import libraries
 // *******************************************
 #include "command_parser.h"
+#include "../operating_environment/environment_variables.h"
 
 #include <sys/wait.h>
 #include <unistd.h>
@@ -9,8 +10,87 @@
 #include <string.h>
 
 
-// internal functions
-// *******************************************
+// REPLACE VARIABLE WITH ACTUAL VALUE
+// **********************************************************************
+// **********************************************************************
+int get_strlen(char *string){
+
+  int index=0;
+
+  while(string[index]!='\0'){
+    index++;
+  }
+
+  return index;
+
+
+}
+
+// **********************************************************************
+char *replace_variables(char *command){
+
+  char *result=malloc(512*sizeof(char));
+  
+  int result_index=0;
+  int command_index=0;
+  int var_start_index=-1;
+  //int var_end_index=-1;
+
+  while(command[command_index]!='\0'){
+
+
+
+    if(command[command_index]!='$'){
+        result[result_index]=command[command_index];
+        result_index++; command_index++;
+
+    }else{
+      //if(command[command_index]=='$')
+
+      // 01_identify variable name
+      command_index++;
+        
+      char arg_buff[100];
+      int buff_index=0;
+
+      while(1){
+
+        char c=command[command_index];
+        int condition=(c>='A'&&c<='Z')||(c>='a'&&c<='z');
+
+        if(condition==1){
+          //printf("\nENTERED******************\n");
+          arg_buff[buff_index]=command[command_index];
+          command_index++; buff_index++;
+        }else{
+
+          break;
+        }
+
+      }
+
+      // get variable from list
+      //printf("\nvar.name = %s", arg_buff);
+      char *value=get_variable(arg_buff).value;
+      //char *value="ALOHA";
+      strcat(result, value);
+      //printf("\nNew Command So Far : %s\n", result);
+
+      result_index+=get_strlen(value);
+
+    }
+
+
+  }
+
+  return result;
+  
+}
+
+
+// IDENTIFY BACKGROUND PROCESS FLAG
+// **********************************************************************
+// **********************************************************************
 int background_flag(char *command){
   
   int ampersand_found=0;
@@ -45,16 +125,22 @@ int background_flag(char *command){
 
 
 
-// interface functions
-// *******************************************
+// INTERFACE FUNCTION
+// **********************************************************************
+// **********************************************************************
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
 
 
 char **parse_command(char *command, int *background_process_flag){
 
+    // 01_indentify background processes
     *background_process_flag=background_flag(command);
 
+    // 02_substitute variables with values if found
+    command=replace_variables(command);
+
+    // 03_parse arguments
     int bufsize = LSH_TOK_BUFSIZE, position = 0;
     char **tokens = malloc(bufsize * sizeof(char*));
     char *token;
